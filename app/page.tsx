6aa3a11d8835afdec0b302b9233df8ad422823e7
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-12-14 00:40:17 +0100

    removed import for link

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
 import { Toaster, toast } from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
