5fca4eb132d30ba874499ed7f47b6cf69a1dc16c
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-12-14 00:42:18 +0100

    modified next config to ignore ts build errors

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
+  typescript: {
+    // Ignore TypeScript build errors
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
