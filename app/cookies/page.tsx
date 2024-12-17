aa95da65c3511f1e250ef1a2b13b2f73ef6f6e2d
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-12-17 10:25:52 +0100

    added ShadCnUi components, Dark&Light Mode added

---
+++ app/cookies/page.tsx
@@ -0,0 +1,14 @@
+export default function Cookiespage() {
+    return (
+        <div className="min-h-screen bg-[#fff] flex flex-col items-center justify-center p-6 ">
+            <div className="w-full max-w-md space-y-8 bg-[#151515] p-8 rounded-2xl shadow-[black] shadow-lg">
+                <div className="flex flex-col items-center">
+                    <h1 className="text-3xl font-bold text-white mb-2">Cookies</h1>
+                    <p className="text-gray-400 text-center mb-8">
+                        This is a sample Next.js app.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
